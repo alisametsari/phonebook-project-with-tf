1ffa48df4b4cfad87d6a8f5aa991ca503bc225fc
ali samet sarı <sari00281@gmail.com>
2023-03-21 14:52:20 +0300

    all files updated

--- user-data.sh
+++ user-data.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 yum update -y
-yum install python3 -y
+yum install pip -y
 pip3 install flask
 pip3 install flask_mysql
 yum install git -y
-TOKEN="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-cd /home/ec2-user && git clone https://$TOKEN@github.com/<account-name>/phonebook.git
-python3 /home/ec2-user/phonebook/phonebook-app.py
+TOKEN=${user-data-git-token}
+USER=${user-data-git-name}
+cd /home/ec2-user && git clone https://$TOKEN@github.com/alisametsari/phonebook-project-with-tf
+python3 /home/ec2-user/phonebook-project-with-tf/phonebook-app.py
